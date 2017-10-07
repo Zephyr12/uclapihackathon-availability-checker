c0d10cc8289ff9312b26d40a3103615746ca7123
ImmanuelBaskaran <immanuel.baskaran.16@ucl.ac.uk>
2017-10-07 15:12:11 +0100

    Added JQuery

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(function() {
+    $("inputfield").append('<button>Jack</button><button>Frost</button>');
+});
+
+$(".button").on("click", function() {
+    var $button = $(this);
+    var oldValue = $button.parent().find("input").val();
+    if ($button.text() == "+") {
+        var newVal = parseFloat(oldValue) + 1;
+    } else {
+        // Don't allow decrementing below zero
+        if (oldValue > 0) {
+            var newVal = parseFloat(oldValue) - 1;
+        } else {
+            newVal = 0;
+        }
+    }
+    $button.parent().find("input").val(newVal);
+
+});
